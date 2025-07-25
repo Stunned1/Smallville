02a6458ec07b3a78c06b5d4835d10fe629455832
Stunned1 <aidan.g124956@gmail.com>
2025-07-24 20:20:03 -0400

    added locations to the project

---
+++ locations.py
@@ -0,0 +1,18 @@
+locations = {
+    "Cafe": { #make sure you add more locations, this is the first location.
+        "description": "cafe",
+        "options": {
+            "1": "location1",
+            "2": "location2",
+            "3": "location3",
+        }
+    },
+    # "location2": {
+    #     "description": "location2",  #These are placeholders for future locations
+    #     "options": {
+    #         "1": "location1",
+    #         "2": "location2",
+    #         "3": "location3",
+    #     }
+    # }
+}
